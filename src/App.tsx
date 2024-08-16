36378201b6560112ca70be2fc526eb1fcd0e3a72
Rojinhas <ruthrojasdasilva12@gmail.com>
2024-08-16 14:01:42 -0300

    task o2

--- src/App.tsx
+++ src/App.tsx
@@ -2,14 +2,22 @@ import './App.css';
 import Home from './pagina/home/Home';
 import Navbar from './componentes/navbar/Navbar';
 import Footer from './componentes/footer/Footer';
-
+import { BrowserRouter, Route, Routes } from 'react-router-dom';
 function App() {
   return (
     <>
-    <Navbar/>
-    <Home/>
-    <Footer/>
+      <BrowserRouter>
+        <Navbar />
+        <div className='min-h-[80vh]'>
+          <Routes>
+            <Route path="/" element={<Home />} />
+            <Route path="/home" element={<Home />} />
+            <Route path="/home" element={<Home />} />
+          </Routes>
+        </div>
+        <Footer />
+      </BrowserRouter>
     </>
-);
+  );
 }
 export default App;
--- src/componentes/navbar/Navbar.tsx
+++ src/componentes/navbar/Navbar.tsx
@@ -1,25 +1,19 @@
-import React from 'react'
-
-
 function Navbar() {
- 
-  
 
   return (
     <>
-     <div className='w-full bg-indigo-900 text-white flex justify-center py-4'>
-          <div className="container flex justify-between text-lg">
-            <div className='text-2xl font-bold uppercase'>Farmacia</div>
-
-            <div className='flex gap-4'>
-              <div className='hover:underline'>Postagens</div>
-              <div className='hover:underline'>Produtos</div>
-              <div className='hover:underline'>Cadastrar produtos</div>
-              <div className='hover:underline'>Perfil</div>
-              <div className='hover:underline'>Sair</div>
-            </div>
+      <div className='w-full bg-indigo-900 text-white flex justify-center py-4'>
+        <div className="container flex justify-between text-lg">
+          <div className='text-2xl font-bold uppercase'>Farmacia</div>
+          <div className='flex gap-4'>
+            <div className='hover:underline'>Postagens</div>
+            <div className='hover:underline'>Produtos</div>
+            <div className='hover:underline'>Cadastrar produtos</div>
+            <div className='hover:underline'>Perfil</div>
+            <div className='hover:underline'>Sair</div>
           </div>
         </div>
+      </div>
     </>
   )
 }
